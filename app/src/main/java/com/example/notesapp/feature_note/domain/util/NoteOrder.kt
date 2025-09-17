0bafd204ac1a91f51f36353929428d7faff3d00f
prisonmike <utamas2003@gmail.com>
2025-09-17 20:19:55 +0200

    added OrderSection to notes componens, new copy method for NoteOrder types

--- app/src/main/java/com/example/notesapp/feature_note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/example/notesapp/feature_note/domain/util/NoteOrder.kt
@@ -2,9 +2,17 @@ package com.example.notesapp.feature_note.domain.util
 
 sealed class NoteOrder(val orderType: OrderType) {
     class Title(orderType: OrderType) : NoteOrder(orderType)
-
     class Date(orderType: OrderType) : NoteOrder(orderType)
-
     class Color(orderType: OrderType) : NoteOrder(orderType)
 
+
+    // ha dataclassok lennének fentebb, akkor lenne alapból copy
+    fun copy(ordertype: OrderType) : NoteOrder {
+        // amikor a jelenlegi noteorder..
+        return when(this) {
+            is Title -> Title(ordertype)
+            is Date -> Date(ordertype)
+            is Color -> Color(ordertype)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/notesapp/feature_note/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,74 @@
+package com.example.notesapp.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.notesapp.feature_note.domain.util.OrderType
+import com.example.notesapp.feature_note.domain.util.NoteOrder
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+
+    // eldönti melyik gomb lett megnyomva
+    noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+
+    // amikor megváltozik az irány, az új NoteOrder-t egy  callback fun ként adjuk át a parent composable-nek
+    onOrderChange: (NoteOrder) -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = "Title",
+                selected = noteOrder is NoteOrder.Title,
+                onSelect = { onOrderChange(NoteOrder.Title(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            DefaultRadioButton(
+                text = "Date",
+                selected = noteOrder is NoteOrder.Date,
+                onSelect = { onOrderChange(NoteOrder.Date(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            DefaultRadioButton(
+                text = "Color",
+                selected = noteOrder is NoteOrder.Color,
+                onSelect = { onOrderChange(NoteOrder.Color(noteOrder.orderType)) }
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = "Ascending",
+                selected = noteOrder.orderType is OrderType.Ascending,
+                onSelect = {
+                    onOrderChange(noteOrder.copy(OrderType.Ascending))
+                }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            DefaultRadioButton(
+                text = "Descending",
+                selected = noteOrder.orderType is OrderType.Descending,
+                onSelect = {
+                    onOrderChange(noteOrder.copy(OrderType.Descending))
+                }
+            )
+        }
+    }
+}
